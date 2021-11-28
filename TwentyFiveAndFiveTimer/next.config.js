42c17fcd875169c80faaf8111ef1b8776bc1562f
tacs <tacsman+git@gmail.com>
2021-11-28 17:14:23 +0000

    static site snapshot test

--- TwentyFiveAndFiveTimer/next.config.js
+++ TwentyFiveAndFiveTimer/next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   reactStrictMode: true,
+  basePath: '/FCC_FrontEndDevelopmentLibraries/TwentyFiveAndFiveTimer/snapshot',
+  assetPrefix: '/FCC_FrontEndDevelopmentLibraries/TwentyFiveAndFiveTimer/snapshot',
 }
